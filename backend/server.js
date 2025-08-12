0ea3f033994430279c83cfa6769377e6cb018ce4
pradeep <narupradeep001@gmail.com>
2025-08-12 22:24:46 +0530

    server.js modifications

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,32 @@
 require('dotenv').config(); // must be FIRST
 
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-const medicalRoutes = require('./routes/medical');
-const app = express();
 
+// Import Routes
+const medicalRoutes = require('./routes/medical');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-
 connectDB();
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/user', require('./routes/user'));
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 app.use('/api/medical', medicalRoutes);
 
+const __dirnameDir = path.resolve();
+app.use(express.static(path.join(__dirnameDir, '/frontend')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirnameDir, '/frontend/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-
-
-import path from 'path';
-
-import express from 'express';
-
-const app = express();
-
-const __dirname = path.resolve();
-
-app.use(express.static(path.join(__dirname, '/frontend')));
-
-app.get('*', (req, res) => {
-
-  res.sendFile(path.join(__dirname, '/frontend/index.html'));
-
-});
