2697fb8d07b49cd8366b7f60e8c0d18cce145b27
aparajithakolluri <140299797+aparajithakolluri@users.noreply.github.com>
2025-08-15 22:48:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,6 @@ app.use(express.static(frontendPath));
 app.get('*', (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
 });
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`✅ Server running on port ${PORT}`));
 
